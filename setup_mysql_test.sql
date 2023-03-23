8abab68e6e21c1286e78d061ff6df6413c30478b
SOME Barsour Evence <sourbar2001@gmail.com>
2023-03-23 18:49:42 +0100

    task 3 and task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,8 +3,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER if NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED By 'hbnb_test_pwd';
-CREATE ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-CREATE SELECT PRIVILEGES ON `performance_schema`.*  TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT PRIVILEGES ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 
 

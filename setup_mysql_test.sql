02e066879bc9913562726c742dc58ce448d29027
SOME Barsour Evence <sourbar2001@gmail.com>
2023-03-23 18:40:03 +0100

    task 3 and task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,10 @@
 -- a script that prepares a MySQL server for the project
 
 
-CREATE DATATBASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER if NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED By 'hbnb_test_pwd';
-CREATE ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
-CREATE SELECT PRIVILEGES ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
+CREATE ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+CREATE SELECT PRIVILEGES ON `performance_schema`.*  TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
+
+

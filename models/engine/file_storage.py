f6198cf2cf41e6d3ebbcfb7f3d77e8756d4fa787
SOME <sourbar2001@gmail.com>
2023-03-23 07:27:53 -0700

    task 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,16 +16,25 @@ class FileStorage:
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
-    def save(self):
+   """ def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
-            json.dump(temp, f)
+            json.dump(temp, f)"""
 
-    def reload(self):
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
+
+
+    """def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
         from models.user import User
@@ -47,10 +56,28 @@ class FileStorage:
                 for key, val in temp.items():
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
-            pass
+            pass"""
 
-    def delete(self, obj=None):
+       
+        def reload(self):
+            """deserializes the JSON file to __objects"""
+            try:
+                with open(self.__file_path, 'r') as f:
+                    jo = json.load(f)
+                for key in jo:
+                    self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+            except:
+                pass
+
+    """def delete(self, obj=None):
         try:
            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
-           pass
+           pass"""
+
+       def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]

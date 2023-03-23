15da47c595bf6a3cc969b443373d3208f95f7dc3
SOME <sourbar2001@gmail.com>
2023-03-23 06:50:11 -0700

    task 2

--- console.py
+++ console.py
@@ -113,7 +113,30 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+
+    def _key_value_parser(self, args):
+        """creates a dictionary from a list of strings"""
+        new_dict = {}
+        for arg in args:
+            if "=" in arg:
+                kvp = arg.split('=', 1)
+                key = kvp[0]
+                value = kvp[1]
+                if value[0] == value[-1] == '"':
+                    value = shlex.split(value)[0].replace('_', ' ')
+                else:
+                    try:
+                        value = int(value)
+                    except:
+                        try:
+                            value = float(value)
+                        except:
+                            continue
+                new_dict[key] = value
+        return new_dict
+
+
+    """def do_create(self, args):
         """ Create an object of any class"""
         arguments = args.split()
         ClassName = args[0]
@@ -143,7 +166,24 @@ class HBNBCommand(cmd.Cmd):
         for key , value in dictionary.items():
              setattr(new_instance, key, value)
         new_instance.save()
-        print(new_instance.id)
+        print(new_instance.id)"""
+
+
+    def do_create(self, arg):
+        """Creates a new instance of a class"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            new_dict = self._key_value_parser(args[1:])
+            instance = classes[args[0]](**new_dict)
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(instance.id)
+        instance.save()
+
 
     def help_create(self):
         """ Help information for the create method """

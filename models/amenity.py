3745049305fd2784498440b60d6c4e0841162cc8
SOME Barsour Evence <sourbar2001@gmail.com>
2023-03-25 01:05:03 +0100

    all task

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey, relationship
 
 
 class Amenity(BaseModel):
-    name = ""
+    __tablename__ = 'amenities'
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                    back_populates="amenities")
+    else:
+        name = ""

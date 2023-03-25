602c998b91215382d5f83b5f009c476d56dc8986
SOME Barsour Evence <sourbar2001@gmail.com>
2023-03-25 10:02:20 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -25,7 +25,7 @@ class BaseModel:
         host = os.getenv('HBNB_MYSQL_HOST')
         db = os.getenv('HBNB_MYSQL_DB')
 
-        self.__engine = create_engine('mysql+pymysql://user:pwd@host/db')
+        self.engine = create_engine('mysql+pymysql://user:pwd@host/db')
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.engine)
 
